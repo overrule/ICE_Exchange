ad40c5716f2065f2b005ca6a7a265b7121209fc3
Everule <everulestorm@gmail.com>
2022-10-30 13:13:59 -0500

    Created stock.rs

---
+++ src/stock.rs
@@ -0,0 +1,19 @@
+mod orderbook;
+use orderbook::Orderbook;
+const MAX_USERS: u32 = 65536;
+struct StockValue{
+    underlying_handle: String,
+}
+impl StockValue{
+    pub fn get_dividends() -> u32{
+        // TODO: impl CF API call and decide dividend function
+        0
+    }
+}
+struct Stock{
+    // TODO: Considering switching to inheritance?
+    orderbook: Orderbook,
+    stock_value: StockValue,
+    ownership: [u32; MAX_USERS]
+}
+
